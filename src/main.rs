4c01f114fb23d9557221063d00ae486b8f713bfa
EricaSantos2109 <erica.santos2110@gmail.com>
2022-12-15 10:41:41 -0300

    projeto 1: formatação do codigo

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,18 @@ fn input() -> String {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
     buf.trim().to_string()
- }
+}
 
-fn create_todo() -> bool{
+fn create_todo() -> bool {
     println!("Gostaria de criar um novo TODO? (s/n)");
     let anwser_todo = input().to_ascii_lowercase();
 
-    if anwser_todo == "s"{
+    if anwser_todo == "s" {
         println!("Qual TODO gostaria de criar?");
         let todo_user = input();
         println!("TODO: {}", todo_user);
         true
-    } else if anwser_todo == "n"{
+    } else if anwser_todo == "n" {
         println!("OK!");
         false
     } else {
